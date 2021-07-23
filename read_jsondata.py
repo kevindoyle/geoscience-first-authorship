541dd2de6f31c046f7bd75c909d0913c2b3bbd08
Laura <laura.ermert@posteo.net>
2021-07-22 21:35:46 -0700

    nothing really...but Analysis_name_authors is cleaned up now

--- read_jsondata.py
+++ read_jsondata.py
@@ -21,4 +21,22 @@ def read_jsons(data_dir, columns=['journal','all_genders', 'all_percent', 'year'
                 df = df.drop(columns=dropcolumns)
                 dfs.append(df)
     df = pd.concat(dfs, ignore_index=True)
-    return (df)
+
+    # clean some journal names
+
+    df.loc[df.journal=='E%26PSL','journal'] = 'EPSL'
+
+    df.loc[df.journal.str.contains("Bulletin"),'journal'] = 'BSSA'
+
+    df.loc[df.journal.str.contains("Seismological"),'journal'] = 'SRL'
+
+    # Remove rows for papers from 2021
+    df = df[~df['year'].isin(['2021'])].copy()
+
+    # add IF
+    dict_IF = {'Nature': 46.486, 'Science': 41.845, 'NatureGeoscience': 16.103, 'EPSL': 4.823, 'GRL': 4.952, 
+        'JGRSolidEarth': 4.191, 'G3': 3.721, 'SRL': 3.131, 'Tectp': 3.048, 'SolidEarth': 2.921, 
+       'GEOPHYSICS': 3.093, 'GJI': 2.834, 'BSSA': 2.274, 'PEPI': 2.413}
+
+    df['IF'] = df['journal'].map(dict_IF)
+    return (df)    
